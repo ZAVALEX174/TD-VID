21d9364d4c2cc189ed24095244dd235e34dc68ec
Alexandr Zuev <zuevalex@yandex.ru>
2023-11-04 01:00:54 +0500

    fixed uniquenes, create go-top button

---
+++ script/script.js
@@ -0,0 +1,31 @@
+// считываем кнопку
+const goTopBtn = document.querySelector(".go-top");
+
+// обработчик на скролл окна
+window.addEventListener("scroll", trackScroll);
+// обработчик на нажатии
+goTopBtn.addEventListener("click", goTop);
+
+function trackScroll() {
+    // вычисляем положение от верхушки страницы
+    const scrolled = window.scrollY ;
+    // считаем высоту окна браузера
+    const coords = document.documentElement.clientHeight;
+    // если вышли за пределы первого окна
+    if (scrolled > coords) {
+        // кнопка появляется
+        goTopBtn.classList.add("go-top--show");
+    } else {
+        // иначе исчезает
+        goTopBtn.classList.remove("go-top--show");
+    }
+}
+
+function goTop() {
+    // пока не вернулись в начало страницы
+    if (window.scrollY  > 0) {
+        // скроллим наверх
+        window.scrollBy(0, -40); // второй аргумент - скорость
+        setTimeout(goTop, 0); // входим в рекурсию
+    }
+}
