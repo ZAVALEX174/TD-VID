751482ee3aa7591b92e09b31d73a1d7154ddd86d
Alexandr Zuev <zuevalex@yandex.ru>
2023-11-19 18:48:52 +0500

    burger close

--- script/burger.js
+++ script/burger.js
@@ -1,7 +1,9 @@
 let menuBtn = document.querySelector('.hamburger-menu');
 let menu = document.querySelector('.menu__list');
+let close = document.querySelector('.hamburger');
 
 menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
+	close.classList.toggle('active')
 })
